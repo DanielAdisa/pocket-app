0193d35ed0daf8f6708726131f5853785c8e227b
Your Name <you@example.com>
2024-10-07 18:43:35 +0100

    asdsad

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,3 +1,6 @@
+import card from "../assets/cardsprwal.png"
+
+
 const Cards = () => {
   return (
     <div className=" md:pl-11 md:pr-11 md:max-w-[1034px] mx-auto md:mx-auto mb-48 p-4">
@@ -12,8 +15,26 @@ const Cards = () => {
             All your transactions in one place
         </p>
         <div className="grid items-center content-center justify-center grid-cols-1 gap-4 mx-auto md:gap-8 md:grid-cols-5">
-            <div className="md:col-span-3 col-span-1 min-h-[600px] bg-[#f0eff2] rounded-[36px] "></div>
-            <div className="md:col-span-2  col-span-1 min-h-[600px] bg-[#f9a70b] rounded-[36px]"> </div>
+
+            <div className="md:col-span-3 col-span-1 max-h-[600px] bg-[#f0eff2] rounded-[36px] p-10 ">
+                <div className="grid grid-rows-2 gap-4">
+                    <div className="row-span-1 ">
+                      <p className="">Connect your physical card to your personal Pocket, Groups, and Shops. You're in complete control.</p>
+                    </div>
+                    <div className="relative flex row-span-1 ">
+                      <img src={card} alt=" " className="-mt-32 md:-mt-52 " />
+                    </div>
+                </div>
+            </div>
+
+
+
+            {/* Orange */}
+            <div className="md:col-span-2  col-span-1 min-h-[600px] bg-[#f9a70b] rounded-[36px] flex">
+              <div className="pt-10 mx-auto">
+                <svg width="5.188em" height="5.188em" viewBox="0 0 83 83" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="83" height="83" rx="32" fill="#fff"></rect><path fill-rule="evenodd" clip-rule="evenodd" d="M62 42c0 11.046-8.954 20-20 20s-20-8.954-20-20 8.954-20 20-20 20 8.954 20 20ZM48.004 52l2-4-5.896-3.962-3.886-.248L38.004 46l4 6h6ZM36.012 36.01 34.002 40l-6.457-4.87C30.115 29.731 35.62 26 41.999 26c1.342 0 2.645.165 3.89.476L44.002 34l-7.99 2.01Z" fill="#FFA400"></path></svg>
+              </div>
+            </div>
         </div>
     </div>
   )
