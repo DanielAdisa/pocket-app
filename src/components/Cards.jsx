60ad4c3294d2a92e3122a03e6f25f11e53072109
Your Name <you@example.com>
2024-10-07 23:53:30 +0100

    asas

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,4 +1,5 @@
 import card from "../assets/cardsprwal.png"
+import phone from "../assets/phoneorange.png"
 
 
 const Cards = () => {
@@ -22,7 +23,7 @@ const Cards = () => {
                       <p className="text-2xl font-bold leading-relaxed">Connect your physical card to your personal Pocket, Groups, and Shops. You're in complete control.</p>
                     </div>
                     <div className="relative flex w-full row-span-1 pb-0 mb-0">
-                      <img src={card} alt=" " className="pb-0 mx-auto mb-0 md:-mt-52" />
+                      <img src={card} alt=" " className="md:pt-[88px] pb-0 mx-auto mb-0 md:-mt-52" />
                     </div>
                 </div>
             </div>
@@ -30,11 +31,12 @@ const Cards = () => {
 
 
             {/* Orange */}
-            <div className="md:col-span-2  col-span-1 min-h-[600px] bg-[#f9a70b] rounded-[36px]">
+            <div className="md:col-span-2  col-span-1 bg-[#f9a70b] rounded-[36px]">
               <div className="flex pt-10 ">
                 <svg className="mx-auto " width="5.188em" height="5.188em" viewBox="0 0 83 83" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="83" height="83" rx="32" fill="#fff"></rect><path fill-rule="evenodd" clip-rule="evenodd" d="M62 42c0 11.046-8.954 20-20 20s-20-8.954-20-20 8.954-20 20-20 20 8.954 20 20ZM48.004 52l2-4-5.896-3.962-3.886-.248L38.004 46l4 6h6ZM36.012 36.01 34.002 40l-6.457-4.87C30.115 29.731 35.62 26 41.999 26c1.342 0 2.645.165 3.89.476L44.002 34l-7.99 2.01Z" fill="#FFA400"></path></svg>
               </div>
-              <p className="text-center">Spend globally with virtual cards</p>
+              <p className="p-10 text-xl font-bold text-center">Spend globally with virtual cards</p>
+              <img src={phone} alt=" " className= " w-[70%] pb-0 mx-auto" />
             </div>
         </div>
     </div>
