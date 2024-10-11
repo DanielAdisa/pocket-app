b70fd19c4f9fda34fe62c66467e687d1b6d9e5cd
Your Name <you@example.com>
2024-10-12 00:06:03 +0100

    com2

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -12,19 +12,19 @@ import phone from "../assets/phonedown.png"
 const Download = () => {
   return (
     <div className=" md:pl-11 md:pr-11 md:w-screen md:max-w-[100vw]  mx-auto md:mx-auto pr-4 pl-4 md:mb-6 ">
-        <div className="w-full wavy md:h-screen md:max-h-[50vh] max-h-[90vh] rounded-[2.5em]  h-screen mx-auto md:w-full p-4 justify-center md:max-w-[80vw] md:p-10 md:pb-0 mb-10 ">
+        <div className="w-full overflow-clip wavy md:h-screen md:max-h-[50vh] max-h-[90vh] rounded-[2.5em]  h-screen mx-auto md:w-full p-6 pb-0 justify-center md:max-w-[80vw]  md:pb-0 mb-10 ">
             <div className="grid w-full h-full md:max-h-[100%]  col-span-1 row-span-2 gap-x-6 md:grid-rows-4 md:grid-cols-6 ">
-                <div className="flex-col w-full md:max-h-[100%] h-full text-center content-start rounded-3xl md:col-span-3 md:row-span-4">
+                <div className="flex-col w-full md:max-h-[100%] h-full text-center md:space-y-7 content-start rounded-3xl md:col-span-3 md:row-span-4">
                     <RiMobileDownloadLine  className="mx-auto text-6xl md:text-8xl "/>
-                    <p className="text-3xl md:text-[2.75em]   text-white">Download LitePay App</p>
-                    <p className="text-xl md:text-[1.7em] md:max-w-[70%] mx-auto text-white">Over 2 million people trust LitePay for their daily money moves, download LitePay today to get started.</p>
-                    <div className="md:flex ">
-                        <p className="pt-4 text-center flex items-center  border-blue-500 border-spacing-96 border-2 gap-2 font-semibold tracking-tight mx-auto w-fit pb-4 pl-8  pr-8 text-[#034b93] rounded-full bg-stone-50"><span><FaGooglePlay className="" /></span>Google Play</p>
+                    <p className="text-3xl md:text-[3em] font-bold   text-white">Download LitePay App</p>
+                    <p className="text-xl md:text-[1.8em] md:max-w-[85%] font-medium mx-auto text-white">Over 2 million people trust LitePay for their daily money moves, download LitePay today to get started.</p>
+                    <div className="md:flex w-[60%] md:space-y-0 space-y-3  mx-auto">
+                        <p className="pt-4 text-center flex items-center  border-blue-500 border-spacing-96  border-2 gap-2 font-semibold tracking-tight mx-auto w-fit pb-4 pl-8  pr-8 text-[#034b93] rounded-full bg-stone-50"><span><FaGooglePlay className="" /></span>Google Play</p>
                         <p className="pt-4 text-center flex items-center  border-blue-500 border-spacing-96 border-2 gap-2 font-semibold tracking-tight mx-auto w-fit pb-4 pl-8  pr-8 text-[#034b93] rounded-full bg-stone-50"><span><FaApple className="" /></span>Apple Store</p>
                     </div>
                 </div>
                 <div className="w-full h-full rounded-3xl md:col-span-3 md:row-span-4">
-                  <div className="flex-col content-end mx-auto pb-0 w-[60%] h-full ">
+                  <div className="flex-col content-end mx-auto pb-0 w-[95%] md:w-[60%] h-full ">
                       <img src={phone} alt="" />
                   </div>
                 </div>
