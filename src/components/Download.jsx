57cab8ebab8f1d810bcca5b4989d1db181401b8b
Your Name <you@example.com>
2024-10-11 17:42:39 +0100

    zxzz

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -1,11 +1,24 @@
+import { RiMobileDownloadFill } from "react-icons/ri";
+import { RiMobileDownloadLine } from "react-icons/ri";
+
 
 
 const Download = () => {
   return (
-    <div className=" md:pl-11 md:pr-11 md:w-screen md:max-w-[80vw]  mx-auto md:mx-auto md:mb-6 pb-10 ">
-        <div className="w-full wavy md:h-screen md:max-h-[60vh] h-[700px] mx-auto md:w-full md:max-w-[80vw] mb-10 ">
-            
+    <div className=" md:pl-11 md:pr-11 md:w-screen md:max-w-[100vw]  mx-auto md:mx-auto pr-4 pl-4 md:mb-6 ">
+        <div className="w-full wavy md:h-screen md:max-h-[90vh] max-h-[90vh] rounded-[2.5em]  h-screen mx-auto md:w-full p-4 justify-center md:max-w-[80vw] md:p-10 md:pb-0 mb-10 ">
+            <div className="grid w-full h-full col-span-1 row-span-2 gap-x-6 md:grid-rows-4 md:grid-cols-6 ">
+                <div className="flex-col w-full h-full text-center rounded-3xl md:col-span-3 md:row-span-4">
+                    <RiMobileDownloadLine  className="mx-auto mb-4 text-6xl md:text-8xl "/>
+                    <p className="text-3xl md:text-[2.75em] text-white">Download LitePay App</p>
+                    <p className="text-xl md:text-[1.4em] text-white">Over 2 million people trust LitePay for their daily money moves, download LitePay today to get started.</p>
+                </div>
+                <div className="w-full h-full bg-black rounded-3xl md:col-span-3 md:row-span-4"></div>
+            </div>
         </div>
+
+
+
         <div className="flex justify-between w-full p-8 pt-6 pb-6 font-semibold rounded-full shadow-lg md:text-xl md:pl-24 md:pr-24 bg-stone-200 h-fit">
             <p className="p-4 pt-1 pb-1 rounded-full hover:bg-stone-800 hover:text-stone-50">FAQs</p>
             <p className="p-4 pt-1 pb-1 rounded-full hover:bg-stone-800 hover:text-stone-50">Privacy</p>
@@ -16,3 +29,5 @@ const Download = () => {
   )
 }
 export default Download
+
+
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,7 +1,7 @@
 import phone2 from "../assets/bg2.png"
 const Services = () => {
   return (
-    <section className=" pt-[90px] md:pt-[125px] md:pl-11 md:pr-11 mb-0 md:max-w-[1034px] h-fit pb-10 mx-auto md:mx-auto p-4">
+    <section className=" pt-[90px] md:pt-[125px] md:pl-11 md:pr-11  md:max-w-[1034px] mx-auto md:mx-auto p-4">
         <div className="">
                 <h2 className=" font-bold text-[3rem] md:text-[4.5rem] tracking-tighter text-[#285183] mb-[20px] leading-[1.25em]">
                     Services
@@ -12,41 +12,41 @@ const Services = () => {
         </div>
 
         <div className="grid md:w-fit md:gap-1.5 rounded-2xl text-stone-50 grid-cols-1 gap-1 mb-20 md:grid-cols-6 md:grid-rows-4 h-screen md:h-screen">
-            <div className="col-span-1  md:col-span-2 w-full h-[108px] rounded-xl md:h-full row-start-1 flex items-center justify-center bg-black">
+            <div className="flex items-center justify-center w-full h-full col-span-1 row-start-1 bg-black md:col-span-2 rounded-xl md:h-full">
                 <h1 className="text-2xl md:text-5xl ">01</h1>
             </div>
 
-            <div className="col-span-1 flex items-center rounded-xl justify-center md:col-span-2 row-start-3 md:col-start-5 md:row-start-2  md:h-full h-[108px] bg-black">
+            <div className="flex items-center justify-center h-full col-span-1 row-start-3 bg-black rounded-xl md:col-span-2 md:col-start-5 md:row-start-2 md:h-full">
                 <h1 className="text-2xl md:text-5xl ">02</h1>
             </div>
 
-            <div className="col-span-1 flex items-center rounded-xl row md:col-span-2 row-start-5 justify-center md:h-full  h-[108px] bg-black md:row-start-3">
+            <div className="flex items-center justify-center h-full col-span-1 row-start-5 bg-black rounded-xl row md:col-span-2 md:h-full md:row-start-3">
                 <h1 className="text-2xl md:text-5xl ">03</h1>
             </div>
 
-            <div className="col-span-1 flex items-center rounded-xl justify-center md:h-full md:col-start-5 md:col-span-2  h-[108px] md:row-start-4 bg-black">
+            <div className="flex items-center justify-center h-full col-span-1 bg-black rounded-xl md:h-full md:col-start-5 md:col-span-2 md:row-start-4">
                 <h1 className="text-2xl md:text-5xl ">04</h1>
             </div>
 
-            <div className="relative flex flex-col justify-end md:text-left   p-6 overflow-hidden w-full h-[108px] md:h-full rounded-xl col-span-1 md:row-start-2 row-start-2 bg-blue-300 md:col-span-4">
+            <div className="relative flex flex-col justify-end w-full h-full col-span-1 row-start-2 p-6 overflow-hidden bg-blue-300 md:text-left md:h-full rounded-xl md:row-start-2 md:col-span-4">
                 <img src={phone2} alt="" class="absolute  blur-sm inset-0 h-full w-full object-cover"/>
                 <div class="absolute inset-0   "></div>
                 <h3 class="z-10  md:text-3xl  font-bold">Get 25 free transfers to International banks every month. </h3>
             </div>
 
-            <div className="relative flex flex-col justify-end md:text-right  p-6 overflow-hidden w-full h-[108px] md:h-full rounded-xl col-span-1 row-start-4 bg-blue-300 md:row-start-1 md:col-span-4">
+            <div className="relative flex flex-col justify-end w-full h-full col-span-1 row-start-4 p-6 overflow-hidden bg-blue-300 md:text-right md:h-full rounded-xl md:row-start-1 md:col-span-4">
                 <img src={phone2} alt="" class="absolute  blur-sm inset-0 h-full w-full object-cover"/>
                 <div class="absolute inset-0  "></div>
                 <h3 class="z-10  md:text-3xl   font-bold">Enjoy cashless payment options online and offline.</h3>
             </div>
 
-            <div className="relative flex flex-col justify-end md:text-left md:col-start-3 p-6 overflow-hidden bg-blue-300 md:h-full rounded-xl h-[108px] row-start-6 md:col-span-4 md:row-start-3">
+            <div className="relative flex flex-col justify-end h-full row-start-6 p-6 overflow-hidden bg-blue-300 md:text-left md:col-start-3 md:h-full rounded-xl md:col-span-4 md:row-start-3">
                 <img src={phone2} alt="" class="absolute  blur-sm inset-0 h-full w-full object-cover"/>
                 <div class="absolute inset-0   "></div>
                 <h3 class="z-10  md:text-3xl   font-bold">Pay your essential bills and buy gift cards easily from one place.</h3>
             </div>
 
-            <div className="relative flex flex-col justify-end md:text-right  p-6 overflow-hidden md:col-span-4 md:h-full rounded-xl  h-[108px] md:row-start-4 bg-blue-300">
+            <div className="relative flex flex-col justify-end h-full p-6 overflow-hidden bg-blue-300 md:text-right md:col-span-4 md:h-full rounded-xl md:row-start-4">
                 <img src={phone2} alt="" class="absolute  blur-sm inset-0 h-full w-full object-cover"/>
                 <div class="absolute inset-0   "></div>
                 <h3 class="z-10  md:text-3xl   font-bold">Save money automatically any time you spend.</h3>
