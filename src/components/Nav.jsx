230481a0f70f70eb09ed9b33d5e1e810cf5aa527
Your Name <you@example.com>
2024-10-16 04:50:03 +0100

    ascac

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,7 @@ import phone from "../assets/Onboarding3.png"
 import card from "../assets/cardmain.png"
 import card1 from "../assets/card2.png"
 import card2 from "../assets/card3.png"
-import logo from "../assets/react.svg"
+import logo from "../assets/logo.svg"
 import { FaGooglePlay } from "react-icons/fa6";
 import { FaAppStoreIos } from "react-icons/fa";
 import { SiAppstore } from "react-icons/si";
@@ -13,20 +13,21 @@ import { motion } from "framer-motion"
 
 const Nav = () => {
   return (
-    <section className="w-full  mx-auto tracking-tight text-white md:w-[1200px] md:max-w-[1200px] font-semibold font-grey-med h-fit">
+    <section className="w-full  mx-auto tracking-tight text-white md:w-[1200px] md:max-w-[1200px] font-grey-med h-fit">
         <nav  className="fixed h-fit z-50 mx-auto shadow-sm bg-transparet md:w-[1200px]">
                 <div className="flex items-center justify-between mx-auto md:max-w-[1200px]">
-                    <div className="flex ml-4">
-                        <p className="rounded-full "><GiPayMoney className="text-3xl " /></p>
+                    <div className="flex items-center justify-center gap-6 ml-4">
+                        <img src={logo} alt="" className=""/>
+                        <p className="rounded-full pt-[3%] pb-[3%] pr-[9%] pl-[9%] bg-white/15 border-[2px] border-white/50  font-grey-lit "> EN </p>
                     </div>
-                    <div className="flex pr-0 p-[2%] gap-x-12 ">
-                        <p className="rounded-full font-Grey hover:cursor-pointer">Product</p>
+                    <div className="flex font-grey-bold pr-0 p-[2%] gap-x-12 ">
+                        <p className="rounded-full font-Grey hover:cursor-pointer">Products</p>
                         <p className="rounded-full hover:cursor-pointer">Learn</p>
                         <p className="rounded-full hover:cursor-pointer">Company</p>
                     </div>
-                    <div className="flex pr-0 p-[2%] gap-x-12 ">
+                    <div className="flex items-center pr-0 p-[2%] gap-x-6 ">
                         <p className="rounded-full hover:cursor-pointer">Log In</p>
-                        <p className="rounded-full hover:cursor-pointer font-meduim">Get Started -- It's free</p>
+                        <p className=" font-meduim pt-[15px] rounded-lg bg-white/15 border-[2px] border-white/50 pb-[15px] pr-[15px] pl-[15px]">Get Started -- It's free</p>
                       
                     </div>
                   
