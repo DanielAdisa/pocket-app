caa6a50fbd2374de348c7204c1aa9d5f57bc50ab
Your Name <you@example.com>
2024-10-17 01:41:21 +0100

    2

--- src/components/AfterGrid.jsx
+++ src/components/AfterGrid.jsx
@@ -2,6 +2,8 @@ import image from "../assets/afterhero.png"
 import { MdPaid } from "react-icons/md";
 import { FaShoppingCart } from "react-icons/fa";import { GiWallet } from "react-icons/gi";
 import { AiOutlineSafetyCertificate } from "react-icons/ai";
+import { IoArrowForwardCircleOutline } from "react-icons/io5";
+
 
 
 
@@ -10,19 +12,19 @@ const AfterGrid = () => {
   return (
     <section className="w-full mx-auto md:w-[1200px] mt-[100px] h-fit md:max-w-[1200px]">
        <div className="flex-col w-full h-fit">
-            <div className="flex w-full p-4 mb-0 bg-blue-500/15 h-fit">
+            <div className="flex w-full p-4 mb-0 grid-blue border-b-0 border-r-0 border-[1.2px] rounded-xl h-fit">
                 <div className=" w-[30%] font-grey-bold leading-[64px] text-[50px]">
-                    <p className="">Fast, Safe, Social Payments</p>
+                    <p className="text-[#5e19b3]">Fast, Safe, Social Payments</p>
                 </div>
-                <div className=" w-[45%] flex-col mb-0 ">
-                    <p className="text-[18px] font-grey-med font-medium leading-[26px] ">Get paid faster, send money securely, unlock free airtime and more, all in one place.</p>
-                    <p className="text-[18px] font-grey-bold pt-[3%] pb-[3%] pr-[2%] text-white text-center mt-4 pl-[2%] w-[35%] rounded-md  bg-black/80 leading-[26px]">Get Spot</p>
+                <div className=" w-[45%] flex-col h-full content-end mt-4 mb-0 ">
+                    <p className="text-[20px] font-grey-reg leading-tight  ">Get paid faster, send money securely, unlock free airtime and more, all in one place.</p>
+                    <p className="text-[19px] flex font-grey-med pt-[3%] pb-[3%] text-[#2467e3] items-center gap-3 text-start mt-4 w-[35%] rounded-md border-[1px] leading-[26px]">Get Spot  <IoArrowForwardCircleOutline /> </p>
                 </div>
             </div>
-            <div className=" bg-blue-500/15 p-4 mt-[10px]">
-               <img src={image} alt="" className=" grayscale hover:grayscale-0" />
+            <div className=" purple-grid border-b-0 border-l-0 border-[1.2px] rounded-xl p-4 mt-[10px]">
+               <img src={image} alt="" className="" />
             </div>
-            <div className="flex w-full bg-blue-500/15 mt-[10px] space-x-4 p-4">
+            <div className="flex w-full text-black/90 greengrid rounded-xl mt-[10px] space-x-4 p-4">
                 <div className="flex-col w-[25%] space-y-3">
                     <MdPaid className="h-[50px] w-[50px] "/>
                     <p className="text-[1.5rem] font-grey-bold">Get Paid</p>
@@ -49,3 +51,4 @@ const AfterGrid = () => {
   )
 }
 export default AfterGrid
+
