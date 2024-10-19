801e218aeaec3102baf539f1d7c936aef48c02cc
Your Name <you@example.com>
2024-10-19 08:48:12 +0100

    5

--- src/components/Pays.jsx
+++ src/components/Pays.jsx
@@ -9,10 +9,10 @@ const Pays = () => {
        <div className="flex items-start justify-between w-full pl-[71.8px] pt-[71.8px] h-full greengrid2 overflow-clip">
             <div className="w-1/3">
                 {/* <img src={image} alt="" className=" w-fit h-[100%]" /> */}
-                <p className="text-[1em] uppercase font-grey-med tracking-[2px] text-[#1b1f28]">Payments</p>
+                <p className="text-[1em] uppercase font-grey-med tracking-[2px] text-black/90">Payments</p>
                 <p className="text-[50px]  -tracking-[1px] font-grey-bold leading-[45px] mt-[40px] text-[#1b1f28]">Send Recieve and Save globally </p>
                 <p className="text-[1rem] w-[95%] font-grey-med text-convertsmall1 text-[#1b1f28] mt-[40px]">Shop securely online at all your favorite stores, and make secure payments for flights, hotels, subscriptions, and more using your virtual Mastercard. </p>
-                <p className="text-[19px] flex font-grey-med text-[#2467e3] items-center gap-3 text-start mt-[100px] w-[35%] leading-[26px]">Get Spot  <IoArrowForwardCircleOutline /> </p>
+                <p className="text-[19px] flex font-grey-med text-white/90 items-center gap-3 text-start mt-[100px] w-[35%] leading-[26px]">Get Spot  <IoArrowForwardCircleOutline /> </p>
             </div>
             <div className="content-end w-2/3">
                 <img src={image} alt="" className="" />
--- src/components/Started.jsx
+++ src/components/Started.jsx
@@ -14,9 +14,9 @@ const Started = () => {
         <div className="flex w-full h-auto gap-[1.5em] ">
             <div className="flex-col w-1/3">
                 <div className="flex transition-all duration-100 ease-in-out delay-150 illustration pr-[1.5em]  -rotate-1 hover:rotate-0">
-                <img src={image2} alt=" " className="self-end " />
+                <img src={image2} alt=" " className="self-end transition-all ease-in-out hover:scale-105 " />
                 </div>
-                <div className="mt-[1em] ">
+                <div className="mt-[1em] illustrationtwo1 p-[1.5em] h-[132px] ">
                     <p className=" font-grey-bold text-center text-[#1b1f28] text-[24px]">Download App</p>
                     <p className="text-center text-[16px] text-[#49546c] mx-auto w-[70%]">Sign up on our web or mobile app with your details.</p>
                 </div>
@@ -24,9 +24,9 @@ const Started = () => {
 
             <div className="flex-col w-1/3">
                 <div className="flex transition-all duration-100 ease-in-out delay-150 p-[1.5em] pb-0 illustrationtwo rotate-1 hover:rotate-0">
-                <img src={image3} alt=" " className="self-end " />
+                <img src={image3} alt=" " className="self-end transition-all ease-in-out hover:scale-105" />
                 </div>
-                <div className="mt-[1em] ">
+                <div className="mt-[1em] illustrationtwo2 p-[1.5em] h-[132px] ">
                     <p className=" font-grey-bold text-center text-[#1b1f28] text-[24px]">Complete your KYC</p>
                     <p className="text-center text-[16px] text-[#49546c] leading-[20px] mx-auto w-[70%]">Sign up on our web or mobile app with your details.</p>
                 </div>
@@ -34,9 +34,9 @@ const Started = () => {
 
             <div className="flex-col w-1/3">
                 <div className="flex transition-all duration-100 ease-in-out delay-150 p-[1.5em] pb-0 illustrationthree -rotate-1 hover:rotate-0">
-                <img src={image4} alt=" " className="self-end " />
+                <img src={image4} alt=" " className="self-end transition-all ease-in-out hover:scale-105 " />
                 </div>
-                <div className="mt-[1em] ">
+                <div className="mt-[1em] illustrationtwo1 p-[1.5em] h-[132px] ">
                     <p className=" font-grey-bold text-center text-[#1b1f28] text-[24px]">Talk to an agent</p>
                     <p className="text-center text-[16px] text-[#49546c] mx-auto w-[70%]">Sign up on our web or mobile app with your details.</p>
                 </div>
